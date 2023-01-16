560e46e81d9c87efba498308d1af2d60d9e87619
Favian <favouriteteo@gmail.com>
2023-01-16 22:56:21 +0800

    feat: pivot table for products & categories

---
+++ database/migrations/2023_01_16_144324_create_product_product_category_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_product_category', function (Blueprint $table) {
+            $table->foreignUuid("product_category_id");
+            $table->foreignUuid("product_id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_category_product');
+    }
+};
