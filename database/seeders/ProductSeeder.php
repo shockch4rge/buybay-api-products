cbcf19090fe7b80674b3ec05b7adcd6ea0a0ead6
Favian <favouriteteo@gmail.com>
2023-01-16 22:57:30 +0800

    refactor: more products seeded

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,6 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory()->count(10)->create();
+        Product::factory()->count(50)->create();
     }
 }
